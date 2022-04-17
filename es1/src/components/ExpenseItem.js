087bcfa6246aee38af02ed39c58a39c9d7e8832c
Francesco <f.valli90@gmail.com>
2022-04-17 22:17:18 +0200

    more jsx code, add basic css style

--- es1/src/components/ExpenseItem.js
+++ es1/src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import './ExpensesItem.css'
+
 function ExpenseItem() {
-  return <p>This is also visible</p>;
+  return (
+    <div className='expense-item'>
+      <div>March 28</div>
+      <div className='expense-item__description'>
+        <h2>Car Insurance</h2>
+        <div className='expense-item__price'>$294.67</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
