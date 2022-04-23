fa021034f9b99e016311604c71d0ad6425314d0d
Francesco <f.valli90@gmail.com>
2022-04-23 22:24:48 +0200

    closer look to useState

--- es1/src/components/Expenses/ExpenseItem.js
+++ es1/src/components/Expenses/ExpenseItem.js
@@ -6,6 +6,7 @@ import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
+  console.log('ExpenseItem evalueted by React')
 
   // function clickHandler() {}
   const clickHandler = () => {
